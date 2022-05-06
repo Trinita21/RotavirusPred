88bbb256c89e5c486b5fed0cc2806de115d2851e
trinita21 <trinita20221@iiitd.ac.in>
2022-05-06 19:31:28 +0530

    help and team page content added

--- help.php
+++ help.php
@@ -1,4 +1,24 @@
 <?php include("header.php"); ?>
+<h1>Welcome To Help Page Of RotavirusPred</h1>
+<p>This web server has been designed to help the scientific community in vaccine design and development. This server predicts zoonotic hosts of Rotavirus A virus from Genome Sequences.
+
+This page provides help on different modules of the server. The web server has a major module: Genome and (ii) Protein. After selecting either of the models, a single or multiple sequences, of either of the 15 proteins or the genome, can be used as an input in .fasta format, after the corresponding protein model is selected by the user.
+
+When a particular model is selected by the user and clicked on the "Predict" button, FluSPred then runs the respective model and provides the result in the form of a table which can be downloaded in .csv format. An example sequence has been provided in the input section.
+
+The result comprises the prediction of the zoonotic host, whether the particular strain would be capable of crossing the species barrier to infect humans or not.</p>
+<h2>Binary Class Prediction</h2>
+<p>Content</p>
+<img src="#" alt="">
+<h2>Result</h2>
+<p>Content</p>
+<img src="#" alt="">
+<h2>Multi Class Prediction</h2>
+<p>Content</p>
+<img src="#" alt="">
+<h2>Result</h2>
+<p>Content</p>
+<img src="#" alt="">
 <?php include("footer.php"); ?>
 </body>
 </html>
--- team.php
+++ team.php
@@ -1,4 +1,5 @@
 <?php include("header.php"); ?>
+<h1>Team of RotavirusPred</h1>
 <?php include("footer.php"); ?>
 </body>
 </html>
